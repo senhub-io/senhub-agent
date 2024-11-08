e6044d12a84086f4de7083aaad5a3b1f58a9ba23
Matthieu Noirbusson <matthieu.noirbusson@sensorfactory.eu>
2024-11-08 21:00:01 +0100

    Add ping and load probes

--- go.mod
+++ go.mod
@@ -14,12 +14,16 @@ require (
 	github.com/getlantern/hidden v0.0.0-20190325191715-f02dbb02be55 // indirect
 	github.com/getlantern/ops v0.0.0-20190325191751-d70cb0d6f85f // indirect
 	github.com/getlantern/systray v1.2.2 // indirect
+	github.com/go-ping/ping v1.1.0 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/google/uuid v1.2.0 // indirect
 	github.com/josephspurrier/goversioninfo v1.4.1 // indirect
 	github.com/ncruces/zenity v0.10.14 // indirect
 	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
 	github.com/randall77/makefat v0.0.0-20210315173500-7ddd0e42c844 // indirect
 	github.com/ybbus/httpretry v1.0.2 // indirect
 	golang.org/x/image v0.20.0 // indirect
+	golang.org/x/net v0.0.0-20210316092652-d523dce5a7f4 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.26.0 // indirect
 )
---
+++ internal/agent/probes/loadWebAppProbe.go
@@ -0,0 +1,88 @@
+package probes
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"net/http"
+	"time"
+
+	"senhub-agent.go/internal/agent/services/configuration"
+	"senhub-agent.go/internal/agent/services/data_store"
+)
+
+type LoadWebAppProbe struct {
+	config 		configuration.RemoteConfiguration
+}
+
+func NewLoadWebAppProbe(config configuration.RemoteConfiguration) Probe {
+	return &LoadWebAppProbe{
+		config: config,
+	}
+}
+
+func (p *LoadWebAppProbe) GetName() string {
+	return "loadWebAppProbe"
+}
+
+func (p *LoadWebAppProbe) ShouldStart() bool {
+	return true
+}
+
+func (p *LoadWebAppProbe) GetInterval() time.Duration {
+	return 30 * time.Second
+}
+
+func (p *LoadWebAppProbe) Collect() ([]data_store.DataPoint, error) {
+	webappURL := "https://tuvalu-legislation.tv"
+
+	domLoadTime, fullLoadTime, err := p.measurePageLoad(webappURL)
+	if err != nil {
+		return nil, fmt.Errorf("Error measuring page load times:", err)
+	}
+
+	return []data_store.DataPoint{
+		{Name: "webApp_domloadtime", Timestamp: time.Now(), Value: float32(domLoadTime)},
+		{Name: "webApp_fullloadtime", Timestamp: time.Now(), Value: float32(fullLoadTime)},
+	}, nil
+}
+
+func (p *LoadWebAppProbe) measurePageLoad(pageURL string) (float32, float32, error) {
+	// Parse the URL to ensure it has the correct format
+	parsedURL, err := url.Parse(pageURL)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	// Ensure the URL has the http or https scheme
+	if parsedURL.Scheme != "http" && parsedURL.Scheme != "https" {
+		fmt.Errorf("invalid URL scheme: %s, must be http or https", parsedURL.Scheme)
+		return 0, 0, fmt.Errorf("invalid URL scheme: %s, must be http or https", parsedURL.Scheme)
+	}
+
+	start := time.Now()
+	resp, err := http.Get(pageURL)
+	if err != nil {
+		return 0, 0, err
+	}
+	defer resp.Body.Close()
+
+	// Simulate DOM load time as half of the total time taken to fetch the page
+	fullLoadTime64 := time.Since(start).Seconds() * 1000 // Convert to milliseconds
+	domLoadTime64 := fullLoadTime64 / 2                     // Estimate DOM load time
+
+	domLoadTime := float32(domLoadTime64)
+	fullLoadTime := float32(fullLoadTime64)
+
+	return domLoadTime, fullLoadTime, nil
+}
+
+
+
+func (p *LoadWebAppProbe) OnStart(quitChannel chan struct{}) error {
+	return nil
+}
+
+func (p *LoadWebAppProbe) OnShutdown(ctx context.Context) error {
+	return nil
+}
