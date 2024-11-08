261b8b4426a60cd6099b4e877b1f26879e568ad1
Matthieu Noirbusson <matthieu@senhub.io>
2024-11-08 22:45:05 +0100

    Make datastore thread safe

--- internal/agent/services/data_store/buffer.go
+++ internal/agent/services/data_store/buffer.go
@@ -1,5 +1,7 @@
 package data_store
 
+import "sync"
+
 type Buffer interface {
 	// Append appends data to the buffer
 	Append(newData []DataPoint) error
@@ -10,7 +12,8 @@ type Buffer interface {
 }
 
 type buffer struct {
-	data *[]DataPoint
+	data  *[]DataPoint
+	mutex sync.Mutex // Ensures thread-safe execution of doRefreshConfig
 }
 
 func NewBuffer() Buffer {
@@ -20,17 +23,26 @@ func NewBuffer() Buffer {
 }
 
 func (b *buffer) Append(newData []DataPoint) error {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+
 	*b.data = append(*b.data, newData...)
 	return nil
 }
 
 func (b *buffer) Sync() []DataPoint {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+
 	data := *b.data
 	*b.data = []DataPoint{}
 	return data
 }
 
 func (b *buffer) AbortSync(failedData []DataPoint) error {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+
 	*b.data = append(*b.data, failedData...)
 	return nil
 }
