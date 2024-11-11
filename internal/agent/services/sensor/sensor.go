0bb168b8b875cdd357a0ded293096f1354eb1877
Matthieu Noirbusson <matthieu.noirbusson@sensorfactory.eu>
2024-11-11 21:08:15 +0100

    Probes are refreshed when config is refreshed

--- internal/agent/services/sensor/sensor.go
+++ internal/agent/services/sensor/sensor.go
@@ -34,14 +34,51 @@ func (s *sensor) GetName() string {
 	return "Sensor"
 }
 
-func (s *sensor) Start(quitChannel chan struct{}) error {
+func (s *sensor) SyncConfiguration() error {
+	validProbeIds := []string{}
 	probeConfigs := s.remoteConfig.GetConfiguration().Probes
 	for _, probeConfig := range probeConfigs {
-		err := s.startProbe(probeConfig, quitChannel)
+		probeId := probes.GenerateProbeId(probeConfig)
+		validProbeIds = append(validProbeIds, probeId)
+
+		// Is there a probe with this configuration already running?
+		for _, startedProbe := range s.startedProbes {
+			if startedProbe.ProbeId == probeId {
+				continue
+			}
+		}
+
+		// Start a new probe poller
+		err := s.startProbe(probeConfig, nil)
 		if err != nil {
 			log.Printf("error starting probe %s: %v", probeConfig, err)
 		}
 	}
+
+	// Stop probes that are no longer in the Configuration
+	for _, startedProbe := range s.startedProbes {
+		found := false
+		for _, validProbeId := range validProbeIds {
+			if startedProbe.ProbeId == validProbeId {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			err := startedProbe.Shutdown(context.Background())
+			if err != nil {
+				log.Printf("error stopping probe %s: %v", startedProbe.GetName(), err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func (s *sensor) Start(quitChannel chan struct{}) error {
+	s.SyncConfiguration()
+	s.remoteConfig.OnConfigChanged(func(string) { s.SyncConfiguration() })
 
 	return nil
 }
