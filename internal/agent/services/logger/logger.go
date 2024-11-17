6a5b3faca816f7f11ddbdf8776a2bf797cfd1654
Matthieu Noirbusson <matthieu.noirbusson@sensorfactory.eu>
2024-11-17 21:18:19 +0100

    Add ability to build in dev or production mode

--- internal/agent/services/logger/logger.go
+++ internal/agent/services/logger/logger.go
@@ -4,13 +4,43 @@ import (
 	"os"
 
 	"github.com/rs/zerolog"
+	agentCliArgs "senhub-agent.go/internal/agent/cliArgs"
 )
 
 type Logger = zerolog.Logger
 
-func NewLogger() *Logger {
+func NewLogger(args *agentCliArgs.ParsedArgs) *Logger {
+	var logger *Logger
+	switch args.Env {
+	case "development":
+		logger = buildDevelopmentLogger()
+	default:
+		logger = buildProductionLogger()
+	}
+
+	if args.Verbose {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+
+	return logger
+}
+
+func buildDevelopmentLogger() *Logger {
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
 	logger := zerolog.
 		New(zerolog.ConsoleWriter{Out: os.Stderr}).
+		With().
+		Timestamp().
+		Logger()
+
+	return &logger
+}
+
+func buildProductionLogger() *Logger {
+	zerolog.SetGlobalLevel(zerolog.WarnLevel)
+
+	logger := zerolog.
+		New(os.Stderr).
 		With().
 		Timestamp().
 		Logger()
