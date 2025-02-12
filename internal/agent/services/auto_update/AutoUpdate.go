3ae04f80a55eade85ca955ca2f22abfbc172b3c5
Matthieu Noirbusson <matthieu@senhub.io>
2025-02-13 00:27:54 +0100

    AutoUpdate simple should update function

--- internal/agent/services/auto_update/AutoUpdate.go
+++ internal/agent/services/auto_update/AutoUpdate.go
@@ -3,9 +3,13 @@ package auto_update
 import (
 	"context"
 
+	"github.com/hashicorp/go-version"
+	"senhub-agent.go/internal/agent/cliArgs"
 	"senhub-agent.go/internal/agent/services/configuration"
 	"senhub-agent.go/internal/agent/services/logger"
 )
+
+var DEFAULT_REGISTRY_URL = "https://eu-west-1.intake.senhub.io/dl/"
 
 // Register an event on remote config change
 // This function checks for update and applies the update if required
@@ -45,4 +49,45 @@ func (a *autoUpdate) Start(quitChannel chan struct{}) error {
 
 func (a *autoUpdate) Shutdown(ctx context.Context) error {
 	return nil
+}
+
+func (a *autoUpdate) shouldUpdate() bool {
+	expectedVersionStr := a.remoteConfig.GetConfiguration().Agent.Version
+	currentVersionStr := cliArgs.Version
+
+	if expectedVersionStr == "" || expectedVersionStr == "latest" {
+		// Assume this is the latest version
+		// Fetch version from the registry
+		expectedVersionStr = cliArgs.Version
+	}
+
+	constraint, err := version.NewConstraint(expectedVersionStr)
+	if err != nil {
+		a.logger.Error().
+			Str("expected_version", expectedVersionStr).
+			Err(err).
+			Msg("Failed to parse version constraint")
+
+		// Unable to parse version constraint
+		// Assume no update required
+		return false
+	}
+
+	currentVersion, err := version.NewVersion(currentVersionStr)
+	if err != nil {
+		a.logger.Error().
+			Str("current_version", currentVersionStr).
+			Err(err).
+			Msg("Failed to parse current version")
+		return false
+	}
+
+	if !constraint.Check(currentVersion) {
+		a.logger.Info().
+			Str("current_version", currentVersionStr).
+			Str("expected_version", expectedVersionStr).
+			Msg("Update required")
+		return true
+	}
+	return false
 }
